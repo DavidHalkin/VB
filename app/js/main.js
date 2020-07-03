740659fef1aea96eb47759a49991cc90b01aa986
David_Halkin <northfire@mail.ru>
2020-07-03 16:51:31 +0300

    dsf

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+	// main mobele nav-menu
+	$('body .header').append('<span class="menu_overlay menu_overlay_js"></span>');
+	$(".mob_nav_btn_js").click(function(){
+        $(".nav_menu_js").toggleClass("opened");
+        $(".menu_overlay_js").toggleClass("active");
+        $("body").toggleClass("overflow-hidden blur");
+    });
+    $(".menu_overlay_js").click(function(){
+        $(".nav_menu_js").toggleClass("opened");
+        $(".menu_overlay_js").toggleClass("active");
+        $("body").toggleClass("overflow-hidden blur");
+    });
+
 	$('.owl_carousel_js').owlCarousel({
 		// loop:true,
 		margin:0,
